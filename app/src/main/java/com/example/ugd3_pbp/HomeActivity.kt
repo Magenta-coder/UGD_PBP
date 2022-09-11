a1056edcc75bedbce20df8d3983cbd9b37130159
Magenta-coder <nicosebastian509@gmail.com>
2022-09-11 18:59:14 +0700

    Menambah Menu Home(Jerry)

---
+++ app/src/main/java/com/example/ugd3_pbp/HomeActivity.kt
@@ -0,0 +1,63 @@
+package com.example.ugd3_pbp
+
+import android.content.DialogInterface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class HomeActivity : AppCompatActivity() {
+
+    private val selected = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.home -> {
+                changeFragment(FragmentHome())
+                true
+            }
+            R.id.obat -> {
+                changeFragment(FragmentSearch())
+                true
+            }
+            R.id.logout -> {
+                val builder: AlertDialog.Builder = AlertDialog.Builder(this@HomeActivity)
+                builder.setMessage("Are you sure want to exit?")
+                    .setPositiveButton("Yes", object : DialogInterface.OnClickListener {
+                        override fun onClick(dialogInterface: DialogInterface, i: Int) {
+                            finishAndRemoveTask()
+                        }
+                    })
+                    .setNegativeButton("No", object : DialogInterface.OnClickListener {
+                        override fun onClick(dialogInterface: DialogInterface, i: Int) {
+                        }
+                    })
+
+                    .show()
+                true
+            }
+            else -> false
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        changeFragment(FragmentHome())
+        val bottomNav: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNav.setOnNavigationItemSelectedListener(selected)
+
+    }
+
+    fun changeFragment(fragment: Fragment?) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.layout_fragment, fragment)
+                .commit()
+        }
+    }
+}
