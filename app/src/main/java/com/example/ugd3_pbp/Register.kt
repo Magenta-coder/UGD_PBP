c4dfb5b0f341e2c2ef98085aab7744ca3a86cb38
Magenta-coder <nicosebastian509@gmail.com>
2022-10-02 13:25:35 +0700

    Menambahkan fitur notif di registrasi
    Signed-off-by: Magenta-coder <nicosebastian509@gmail.com>

--- app/src/main/java/com/example/ugd3_pbp/Register.kt
+++ app/src/main/java/com/example/ugd3_pbp/Register.kt
@@ -1,10 +1,18 @@
 package com.example.ugd3_pbp
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.ugd3_pbp.databinding.ActivityRegisterBinding
 import com.example.ugd3_pbp.room.Obat
 import com.example.ugd3_pbp.room.ObatDB
@@ -27,6 +35,12 @@ class Register : AppCompatActivity() {
 
     val db by lazy { userDB(this) }
 
+    private var bindings: ActivityRegisterBinding? = null
+    private val CHANNEL_ID_1 = "channel_notification_01"
+    private val CHANNEL_ID_2 = "channel_notification_02"
+    private val notificationId1 = 101
+    private val notificationId2 = 102
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -44,6 +58,8 @@ class Register : AppCompatActivity() {
 
 
         val mBundle = Bundle()
+
+        createNotificationChannel()
 
         btnRegis.setOnClickListener(View.OnClickListener{
 
@@ -82,6 +98,7 @@ class Register : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             intent.putExtras(mBundle)
 
+            sendNotification1(username.text.toString())
                 CoroutineScope(Dispatchers.IO).launch {
                     db.userDao().addUser(
                         userData(0,username.text.toString(),
@@ -94,4 +111,54 @@ class Register : AppCompatActivity() {
             startActivity(intent)
         })
     }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Notification Title"
+            val descriptionText = "Notification Description"
+
+            val channel1 = NotificationChannel(CHANNEL_ID_1, name, NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = descriptionText
+            }
+
+            val channel2 = NotificationChannel(CHANNEL_ID_2, name, NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = descriptionText
+            }
+
+            val notificationManager : NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel1)
+            notificationManager.createNotificationChannel(channel2)
+        }
+    }
+
+    private fun sendNotification1(name:String) {
+        //Ini untuk ketika dipencent notifnya, dia ke main activity lagi
+        val intent : Intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val broadcastIntent: Intent = Intent(this, NotificationReceiver::class.java)
+        broadcastIntent.putExtra("toastMessage", bindings?.username2?.text.toString())
+
+        val actionIntent = PendingIntent.getBroadcast(this, 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID_1)
+            .setSmallIcon(R.drawable.ic_email_24)
+            .setContentTitle("Berhasil Registrasi")
+            .setContentText("Selamat Datang " + name)
+            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+            .setColor(Color.GREEN)
+            .setAutoCancel(true)
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+            .addAction(R.mipmap.ic_launcher, "Toast", actionIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(notificationId1, builder.build())
+        }
+    }
+
 }
