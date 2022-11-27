18f055f2bab95dc509f2edb01b05a8de723696b7
Hubertus13 <jubertusnova@gmail.com>
2022-11-27 19:34:42 +0700

    penyelesaian pdf

--- app/src/main/java/com/example/ugd3_pbp/PDFActivity.kt
+++ app/src/main/java/com/example/ugd3_pbp/PDFActivity.kt
@@ -17,8 +17,7 @@ import com.itextpdf.text.Paragraph
 import com.itextpdf.text.pdf.BarcodeQRCode
 import com.itextpdf.text.pdf.PdfDocument
 import com.itextpdf.text.pdf.PdfWriter
-import com.lowagie.text.Cell
-import com.lowagie.text.alignment.HorizontalAlignment
+
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileNotFoundException
@@ -65,74 +64,75 @@ class PDFActivity : AppCompatActivity() {
         }
     }
 
-//    @SuppressLint("ObsoleteSdkInt")
-//    @RequiresApi(api = Build.VERSION_CODES.O)
-//    @Throws(FileNotFoundException::class)
-//
-//    private fun createPdf(nama: String, umur: String, tlp: String, alamat: String, kampus: String) {
-//        val pdfPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString()
-//        val file = File(pdfPath, "pdf_10794.pdf")
-//        FileOutputStream(file)
-//
-//        val writer = PdfWriter(file)
-//        val pdfDocument = PdfDocument(writer)
-//        val document = Document(pdfDocument)
-//        document.setMargins(5f, 5f, 5f, 5f)
-//        @SuppressLint("UseCompatLoadingForDrawables") val d = getDrawable(R.drawable.heart)
-//
-//        val bitmap = (d as BitmapDrawable?)!!.bitmap
-//        val stream = ByteArrayOutputStream()
-//        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
-//        val bitmapdata = stream.toByteArray()
-//        val imageData = ImageDataFactor.create(bitmapdata)
-//        val image = Image(imageData)
-//        val namapengguna = Paragraph("Identitas Pengguna").alignment
-//        val group = Paragraph (
-//            """
-//                Berikut adalah
-//                Nama Pengguna UAJY 2022/2023
-//            """.trimIndent()).setTextAlignment(TextAlignment.CENTER).setFontSize(12f)
-//
-//        val width = floatArrayOf(100f, 100f)
-//        val table = Table(width)
-//
-//        table.setHorizontalAlignment(HorizontalAlignment.CENTER)
-//        table.addCell(Cell().add(Paragraph("Nama Diri")))
-//        table.addCell(Cell().add(Paragraph(nama)))
-//        table.addCell(Cell().add(Paragraph("Umur")))
-//        table.addCell(Cell().add(Paragraph(umur)))
-//        table.addCell(Cell().add(Paragraph("No  Telepon")))
-//        table.addCell(Cell().add(Paragraph(tlp)))
-//        table.addCell(Cell().add(Paragraph("Alamat Domisili")))
-//        table.addCell(Cell().add(Paragraph(alamat)))
-//        table.addCell(Cell().add(Paragraph("Nama Kampus")))
-//        table.addCell(Cell().add(Paragraph(kampus)))
-//        val dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
-//        table.addCell(Cell().add(Paragraph("Tanggal Buat PDF")))
-//        table.addCell(Cell().add(Paragraph(LocalDate.now().format(dateTimeFormatter))))
-//        val timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss a")
-//        table.addCell(Cell().add(Paragraph("Pukul Pembuatan ")))
-//        table.addCell(Cell().add(Paragraph(LocalTime.now().format(timeFormatter))))
-//
-//        val barcodeQRCode = BarcodeQRCode("""
-//            $nama
-//            $umur
-//            $tlp
-//            $alamat
-//            $kampus
-//            ${LocalDate.now().format(dateTimeFormatter)}
-//            ${LocalTime.now().format(timeFormatter)}
-//        """.trimIndent())
-//
-//        val qrCodeObject = barcodeQRCode.createFormXObject(ColorConstants.BLACK, pdfDocument)
-//        val qrCodeImage = Image(qrCodeObject).setWidth(80f).setHorizontalAlignment(HorizontalAlignment.CENTER)
-//        document.add(image)
-//        document.add(namapengguna)
-//        document.add(group)
-//        document.add(table)
-//        document.add(qrCodeImage)
-//        document.close()
-//        Toast.makeText(applicationContext, "PDF Created", Toast.LENGTH_SHORT).show()
-//    }
+   @SuppressLint("ObsoleteSdkInt")
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Throws(FileNotFoundException::class)
+   
+   private fun createPdf(nama: String, umur: String, tlp: String, alamat: String, obat: String) {
+        val pdfPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString()
+        val file = File(pdfPath, "Data_Customer_Apotek.pdf")
+        FileOutputStream(file)
+
+        val writer = PdfWriter(file)
+        val pdfDocument = PdfDocument(writer)
+        val document = Document(pdfDocument)
+        document.setMargins(5f, 5f, 5f, 5f)
+        @SuppressLint("UseCompatLoadingForDrawables") val d = getDrawable(R.drawable.heart)
+
+        val bitmap = (d as BitmapDrawable?)!!.bitmap
+        val stream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+        val bitmapdata = stream.toByteArray()
+        val imageData = ImageDataFactor.create(bitmapdata)
+        val image = Image(imageData)
+        val namapengguna = Paragraph("Identitas Pengguna").alignment
+        val group = Paragraph (
+            """
+                Berikut adalah
+                Nama Pengguna Aplikasi Apoteker
+            """.trimIndent()).setTextAlignment(TextAlignment.CENTER).setFontSize(12f)
+
+       val width = floatArrayOf(100f, 100f)
+        val table = Table(width)
+
+        table.setHorizontalAlignment(HorizontalAlignment.CENTER)
+        table.addCell(Cell().add(Paragraph("Nama Diri")))
+        table.addCell(Cell().add(Paragraph(nama)))
+        table.addCell(Cell().add(Paragraph("Umur")))
+        table.addCell(Cell().add(Paragraph(umur)))
+        table.addCell(Cell().add(Paragraph("No  Telepon")))
+        table.addCell(Cell().add(Paragraph(tlp)))
+        table.addCell(Cell().add(Paragraph("Alamat Domisili")))
+        table.addCell(Cell().add(Paragraph(alamat)))
+        table.addCell(Cell().add(Paragraph("Nama Obat")))
+        table.addCell(Cell().add(Paragraph(obat)))
+
+        val dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+        table.addCell(Cell().add(Paragraph("Tanggal Buat PDF")))
+        table.addCell(Cell().add(Paragraph(LocalDate.now().format(dateTimeFormatter))))
+        val timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss a")
+        table.addCell(Cell().add(Paragraph("Pukul Pembuatan ")))
+        table.addCell(Cell().add(Paragraph(LocalTime.now().format(timeFormatter))))
+
+        val barcodeQRCode = BarcodeQRCode("""
+            $nama
+            $umur
+            $tlp
+            $alamat
+            $obat
+            ${LocalDate.now().format(dateTimeFormatter)}
+            ${LocalTime.now().format(timeFormatter)}
+        """.trimIndent())
+
+        val qrCodeObject = barcodeQRCode.createFormXObject(ColorConstants.BLACK, pdfDocument)
+        val qrCodeImage = Image(qrCodeObject).setWidth(80f).setHorizontalAlignment(HorizontalAlignment.CENTER)
+        document.add(image)
+        document.add(namapengguna)
+        document.add(group)
+        document.add(table)
+        document.add(qrCodeImage)
+        document.close()
+        Toast.makeText(applicationContext, "PDF Created", Toast.LENGTH_SHORT).show()
+    }
 
 }
